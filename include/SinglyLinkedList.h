2ab567dc3b48096d744ef39e6653d9d103e492e1
Keshav Sahu <47982459+KSCreator@users.noreply.github.com>
2020-07-19 22:00:42 +0530

    Create SinglyLinkedList.h

---
+++ include/SinglyLinkedList.h
@@ -0,0 +1,43 @@
+#ifndef SINGLY_LINKED_LIST
+#define SINGLY_LINKED_LIST
+
+struct SLL_Node{
+  int element;
+  struct SLL_Node *next;
+};
+
+typedef struct SLL_Node SLL_Node;
+
+typedef struct{
+  SLL_Node * head;
+  SLL_Node * tail;
+  size_t length;
+}SinglyLinkedList;
+
+//
+SLL_Node* SLL_CreateNode( int );
+//
+void SLL_Init( SinglyLinkedList* );
+//
+void SLL_AddAtFront( SinglyLinkedList*, int );
+//
+void SLL_Append( SinglyLinkedList*, int );
+//
+void SLL_InsertBefore( SinglyLinkedList*, int, int );
+//
+void SLL_InsertAfter( SinglyLinkedList*, int, int );
+
+//
+void SLL_RemoveFromFront( SinglyLinkedList* );
+//
+void SLL_RemoveFromBack( SinglyLinkedList* );
+//
+void SLL_RemoveAfter( SinglyLinkedList*, int );
+//not implemented yet
+void SLL_RemoveBefore( SinglyLinkedList*, int );
+//
+void SLL_Dispose(SinglyLinkedList*);
+//
+void SLL_ForEach(const SinglyLinkedList *list,void (*) (int));
+
+#endif    //SINGLY_LINKED_LIST
