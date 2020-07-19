43d09ef7482c35443a07f57db12c0f24868d9899
Keshav Sahu <47982459+KSCreator@users.noreply.github.com>
2020-07-19 21:59:07 +0530

    Create main.c

---
+++ test/main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "SinglyLinkedList.h"
+
+void print(int a)
+{
+	printf("%d ",a);
+}
+int main(void) {
+  SinglyLinkedList list;
+  SLL_Init(&list);
+  SLL_Append(&list,100);
+  SLL_Append(&list,200);
+  SLL_Append(&list,300);
+  printf("This list contains\n[");
+  SLL_ForEach(&list,print);
+  printf("]\n");
+  SLL_Dispose(&list);
+  return 0;
+}
