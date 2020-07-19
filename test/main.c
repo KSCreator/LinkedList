bcdf219c9f1928fb5db13a2ca885185e2c61ed5c
Keshav Sahu <Keshavsamsung999@gmail.com>
2020-07-19 18:51:00 +0000

    little change in test\main.c and .replit

--- test/main.c
+++ test/main.c
@@ -5,14 +5,15 @@ void print(int a)
 {
 	printf("%d ",a);
 }
+
 int main(void) {
   DoublyLinkedList list;
   DLL_Init(&list);
   DLL_AddAtFront(&list,100);
   DLL_AddAtFront(&list,200);
   DLL_AddAtFront(&list,300);
-  DLL_Dispose(&list);
   DLL_ForEach(&list,print);
+  DLL_Dispose(&list);
   return 0;
 }
 
